ac82b8c1ac2618b44a43b05671f12cd7599e5f0c
dipakrasal2009 <dipakrasal2009@gmail.com>
2025-07-23 11:50:21 +0530

    change tha application files and deployment files

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "Waiting for database to be ready..."
-sleep 5  # wait for DB container to start
+#echo "Waiting for database to be ready..."
+#sleep 5  # wait for DB container to start
 
-echo "Running DB migrations..."
-python -c "from app import create_app; from app.db import db; app = create_app(); \
-with app.app_context(): db.create_all()"
+#echo "Running DB migrations..."
+#python -c "from app import create_app; from app.db import db; app = create_app(); \
+#with app.app_context(): db.create_all()"
 
 echo "Starting Flask app..."
-exec flask run --host=0.0.0.0 --port=5000
+export FLASK_APP=app:app
+exec flask run --host=0.0.0.0 --port=5050
+
+
+#exec flask run --host=0.0.0.0 --port=5050
 
--- app/project/app/config.py
+++ app/project/app/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     DB_USER = os.getenv('DB_USER', 'user')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
-    DB_HOST = os.getenv('DB_HOST', 'postgres')   # service name from docker-compose
+    DB_HOST = os.getenv('DB_HOST', 'postgres')
     DB_NAME = os.getenv('DB_NAME', 'studentdb')
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
